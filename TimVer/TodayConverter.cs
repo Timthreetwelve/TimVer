3c1c673c41dbb3f948349af239f0f91bc58e919e
Tim <KennedyTim@outlook.com>
2022-01-18 19:32:28 -0600

    Make history row bold if date is today

---
+++ TimVer/TodayConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Data;
+
+namespace TimVer;
+
+internal class TodayConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string)
+        {
+            DateTime dt = DateTime.ParseExact(value.ToString(), "yyyy/MM/dd HH:mm", null);
+            if (dt.Date == DateTime.Today)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
