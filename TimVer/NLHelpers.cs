c4ab26dddb55fddfeb7165f7bcf9c46bb1d884dd
Tim Kennedy <KennedyTim@outlook.com>
2022-08-17 20:05:15 -0500

    Move log file to folder in temp directory

--- TimVer/NLHelpers.cs
+++ TimVer/NLHelpers.cs
@@ -69,7 +69,7 @@ internal static class NLHelpers
 
         // combine temp folder with filename
         string tempdir = Path.GetTempPath();
-        return Path.Combine(tempdir, filename);
+        return Path.Combine(tempdir, "T_K", filename);
     }
     #endregion Create a filename in the temp folder
 
