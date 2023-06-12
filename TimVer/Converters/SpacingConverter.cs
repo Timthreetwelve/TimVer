3a3ba3462919a6fd889c129bf9ccb916fddf498e
Tim Kennedy <kennedytim@outlook.com>
2023-06-12 17:14:20 -0500

    Added Environment Variables page

--- TimVer/Converters/SpacingConverter.cs
+++ TimVer/Converters/SpacingConverter.cs
@@ -10,14 +10,18 @@ internal class SpacingConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
+        if (value == null)
+        {
+            return null;
+        }
         switch (UserSettings.Setting.RowSpacing)
         {
             case Spacing.Compact:
                 return new Thickness(15, 2, 15, 1);
             case Spacing.Comfortable:
-                return new Thickness(15, 4, 15, 3);
+                return new Thickness(15, 6, 15, 6);
             default:
-                return new Thickness(15, 8, 15, 7);
+                return new Thickness(15, 9, 15, 9);
         }
     }
 
