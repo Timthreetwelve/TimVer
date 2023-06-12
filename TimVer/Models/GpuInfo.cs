0e9405c9026d64d1d836a5a7039e802a3e3baeb5
Tim Kennedy <kennedytim@outlook.com>
2023-06-12 17:11:49 -0500

    Added graphics page

---
+++ TimVer/Models/GpuInfo.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
+
+namespace TimVer.Models;
+
+public partial class GpuInfo : ObservableObject
+{
+    [ObservableProperty]
+    private string _gpuName;
+
+    [ObservableProperty]
+    private string _gpuDeviceID;
+
+    [ObservableProperty]
+    private string _gpuDescription;
+
+    [ObservableProperty]
+    private string _gpuNumberOfColors;
+
+    [ObservableProperty]
+    private string _gpuHorizontalResolution;
+
+    [ObservableProperty]
+    private string _gpuVerticalResolution;
+
+    [ObservableProperty]
+    private string _gpuCurrentRefresh;
+
+    [ObservableProperty]
+    private string _gpuMinRefresh;
+
+    [ObservableProperty]
+    private string _gpuMaxRefresh;
+
+    [ObservableProperty]
+    private string _gpuAdapterRam;
+
+    [ObservableProperty]
+    private string _gpuBitsPerPixel;
+
+    [ObservableProperty]
+    private string _gpuVideoProcessor;
+
+    [ObservableProperty]
+    private string _gpuVideoDescription;
+}
---
+++ TimVer/Views/VideoPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
+
+namespace TimVer.Views;
+
+public partial class VideoPage : UserControl
+{
+    public VideoPage()
+    {
+        InitializeComponent();
+    }
+}
