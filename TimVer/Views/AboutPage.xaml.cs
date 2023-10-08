af934c7c69dac0531736af69e1d5f686a3c3fb73
Tim Kennedy <kennedytim@outlook.com>
2023-10-08 07:59:50 -0500

    Handle mouse down in ListView

--- TimVer/Views/AboutPage.xaml.cs
+++ TimVer/Views/AboutPage.xaml.cs
@@ -29,4 +29,16 @@ public partial class AboutPage : UserControl
         e.Handled = true;
     }
     #endregion Clicked on the GitHub link
+
+    #region Mouse down in ListView
+    /// <summary>
+    /// Handle mouse down by doing nothing
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void ListView_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        e.Handled = true;
+    }
+    #endregion Mouse down in ListView
 }
