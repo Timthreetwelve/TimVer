d88bbc268d5cd95da3b6508d4e21a0f05f1c440b
Tim Kennedy <timthreetwelve@outlook.com>
2023-12-15 11:14:43 -0600

    Update converter to work with both Free and Size

--- TimVer/Converters/BoolToStringConverter.cs
+++ TimVer/Converters/BoolToStringConverter.cs
@@ -2,15 +2,26 @@
 
 namespace TimVer.Converters;
 
-/// <summary>
-/// Converts a boolean value of true to "GiB" and false to "GB"
-/// </summary>
 internal class BoolToStringConverter : IValueConverter
 {
+    /// <summary>
+    /// Converts a boolean value of true to "GiB" and false to "GB".
+    /// </summary>
+    /// <remarks>Same converter is used for the Size and Free column headings</remarks>
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         bool boolValue = value is bool? && (bool)value;
         string parm = (string)parameter;
+        if (parm.Equals("Free", StringComparison.OrdinalIgnoreCase))
+        {
+            string free = GetStringResource("DriveInfo_Free");
+            return boolValue ? $"{free} (GiB)" : $"{free} (GB)";
+        }
+        else if (parm.Equals("Size", StringComparison.OrdinalIgnoreCase))
+        {
+            string size = GetStringResource("DriveInfo_Size");
+            return boolValue ? $"{size} (GiB)" : $"{size} (GB)";
+        }
         return boolValue ? $"{parm} (GiB)" : $"{parm} (GB)";
     }
 
