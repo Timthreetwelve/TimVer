a192bf78f1521cdb13bfa73cf79cd9ad8f3c27e4
Tim Kennedy <timthreetwelve@outlook.com>
2023-12-17 14:25:55 -0600

    Replaced ListView with DataGrid.

--- TimVer/Views/AboutPage.xaml.cs
+++ TimVer/Views/AboutPage.xaml.cs
@@ -29,16 +29,4 @@ public partial class AboutPage : UserControl
         e.Handled = true;
     }
     #endregion Clicked on the GitHub link
-
-    #region Mouse down in ListView
-    /// <summary>
-    /// Handle mouse down by doing nothing
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    private void ListView_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-    {
-        e.Handled = true;
-    }
-    #endregion Mouse down in ListView
 }
