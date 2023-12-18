a2c630316aa343d5b0ff84c2d4063aeee2666ef9
Tim Kennedy <timthreetwelve@outlook.com>
2023-12-17 21:43:00 -0600

    Updated views to accommodate wrapping of longer strings

--- TimVer/Views/AboutPage.xaml.cs
+++ TimVer/Views/AboutPage.xaml.cs
@@ -10,23 +10,5 @@ public partial class AboutPage : UserControl
     public AboutPage()
     {
         InitializeComponent();
-
-        txtBuildDate.Text = $"{BuildInfo.BuildDateUtc:f}  (UTC)";
     }
-
-    #region Clicked on the GitHub link
-    /// <summary>
-    /// Opens the default browser to the GitHub page .
-    /// </summary>
-    /// <param name="sender">The sender.</param>
-    /// <param name="e">The <see cref="RequestNavigateEventArgs"/> instance containing the event data.</param>
-    private void OnNavigate(object sender, RequestNavigateEventArgs e)
-    {
-        Process p = new();
-        p.StartInfo.FileName = e.Uri.AbsoluteUri;
-        p.StartInfo.UseShellExecute = true;
-        p.Start();
-        e.Handled = true;
-    }
-    #endregion Clicked on the GitHub link
 }
