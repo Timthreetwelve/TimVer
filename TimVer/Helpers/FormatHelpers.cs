f4845db530355257fa1a7272dc447285a8d360ed
Tim Kennedy <timthreetwelve@outlook.com>
2024-01-21 22:39:53 -0600

    Replaced TextBlock Runs with property

---
+++ TimVer/Helpers/FormatHelpers.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
+
+namespace TimVer.Helpers;
+
+internal static class FormatHelpers
+{
+    public static string FormattedProcessorCores
+    {
+        get
+        {
+            string hCores = GetStringResource("HardwareInfo_Cores");
+            string hThreads = GetStringResource("HardwareInfo_Threads");
+            return $"{CombinedInfo.ProcCores} {hCores} - {CombinedInfo.ProcThreads} {hThreads}";
+        }
+    }
+
+    public static string FormattedMemory
+    {
+        get
+        {
+            string hUsable = GetStringResource("HardwareInfo_Usable");
+            string hTotal = GetStringResource("HardwareInfo_Installed");
+            return $"{CombinedInfo.InstalledMemory} {hTotal} - {CombinedInfo.TotalMemory} {hUsable}";
+        }
+    }
+
+    public static string FormattedBiosVersionDate
+    {
+        get
+        {
+            string name = BiosHelpers.GetBiosVersion();
+            string date = BiosHelpers.GetBiosDate().ToString("d", CultureInfo.CurrentCulture);
+            return $"{name}   {date}";
+        }
+    }
+}
