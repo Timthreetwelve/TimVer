af61a458dcf97a32f7cb403c9118391f21eebb83
Tim Kennedy <timthreetwelve@outlook.com>
2024-04-20 20:45:22 -0500

    Added FindChild method

--- TimVer/Helpers/MainWindowUIHelpers.cs
+++ TimVer/Helpers/MainWindowUIHelpers.cs
@@ -203,6 +203,12 @@ internal static class MainWindowUIHelpers
     #endregion Change mouse pointer
 
     #region Find the parent of a control
+    /// <summary>
+    /// Finds the Parent of the given item in the visual tree.
+    /// </summary>
+    /// <typeparam name="T">The type of the queried item.</typeparam>
+    /// <param name="child">x:Name or Name of child.</param>
+    /// <returns>The parent object.</returns>
     public static T FindParent<T>(DependencyObject child) where T : DependencyObject
     {
         //get parent item
@@ -219,4 +225,57 @@ internal static class MainWindowUIHelpers
             return FindParent<T>(parentObject);
     }
     #endregion Find the parent of a control
+
+    #region Find a child of a control
+    /// <summary>
+    /// Finds a Child of a given item in the visual tree.
+    /// </summary>
+    /// <param name="parent">A direct parent of the queried item.</param>
+    /// <typeparam name="T">The type of the queried item.</typeparam>
+    /// <param name="childName">x:Name or Name of child. </param>
+    /// <returns>The first child item that matches the submitted type parameter.</returns>
+    public static T FindChild<T>(DependencyObject parent, string childName)
+       where T : DependencyObject
+    {
+        // Confirm parent and childName are valid. 
+        if (parent == null)
+            return null;
+
+        T foundChild = null;
+
+        int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+        for (int i = 0; i < childrenCount; i++)
+        {
+            DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+
+            // If the child is not of the request child type child
+            if (child is not T)
+            {
+                // recursively drill down the tree
+                foundChild = FindChild<T>(child, childName);
+
+                // If the child is found, break so we do not overwrite the found child. 
+                if (foundChild != null)
+                    break;
+            }
+            else if (!string.IsNullOrEmpty(childName))
+            {
+                // If the child's name is set for search
+                if (child is FrameworkElement frameworkElement && frameworkElement.Name == childName)
+                {
+                    // if the child's name is of the request name
+                    foundChild = (T)child;
+                    break;
+                }
+            }
+            else
+            {
+                // child element found.
+                foundChild = (T)child;
+                break;
+            }
+        }
+        return foundChild;
+    }
+    #endregion Find a child of a control
 }
