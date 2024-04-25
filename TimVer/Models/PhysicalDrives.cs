df2b24409bf65780e3f737842ac0f59937ab9f30
Tim Kennedy <timthreetwelve@outlook.com>
2024-04-24 21:06:46 -0500

    Sorted properties
    (like they should have been in the first place)

--- TimVer/Models/PhysicalDrives.cs
+++ TimVer/Models/PhysicalDrives.cs
@@ -5,59 +5,62 @@ namespace TimVer.Models;
 public partial class PhysicalDrives : ObservableObject
 {
     [ObservableProperty]
-    private uint _index;
-
-    [ObservableProperty]
-    private string _status;
-
-    [ObservableProperty]
-    private uint _partitions;
-
-    [ObservableProperty]
-    private string _name;
-
-    [ObservableProperty]
-    private string _interface;
-
-    [ObservableProperty]
-    private double _size;
-
-    [ObservableProperty]
-    private string _mediaType;
-
-    [ObservableProperty]
-    private string _model;
+    private string _busType;
 
     [ObservableProperty]
     private string _diskType;
 
     [ObservableProperty]
-    private string _health;
-
-    [ObservableProperty]
-    private string _serialNumber;
-
-    [ObservableProperty]
-    private string _pNPDeviceID;
-
-    [ObservableProperty]
     private string _firmwareRevision;
 
     [ObservableProperty]
-    private string _busType;
+    private string _friendlyName;
 
     [ObservableProperty]
-    private string _location;
+    private string _health;
 
     [ObservableProperty]
-    private string _message;
+    private uint _index;
 
     [ObservableProperty]
-    private string _partitionStyle;
+    private string _interface;
 
     [ObservableProperty]
     private string _isBoot;
 
     [ObservableProperty]
     private string _isSystem;
+
+    [ObservableProperty]
+    private string _location;
+
+    [ObservableProperty]
+    private string _mediaType;
+
+    [ObservableProperty]
+    private string _message;
+
+    [ObservableProperty]
+    private string _model;
+
+    [ObservableProperty]
+    private string _name;
+
+    [ObservableProperty]
+    private uint _partitions;
+
+    [ObservableProperty]
+    private string _partitionStyle;
+
+    [ObservableProperty]
+    private string _pNPDeviceID;
+
+    [ObservableProperty]
+    private string _serialNumber;
+
+    [ObservableProperty]
+    private double _size;
+
+    [ObservableProperty]
+    private string _status;
 }
