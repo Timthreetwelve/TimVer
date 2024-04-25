c5b377b04c51d6afe0a2d0a34c2bd3e16777c557
Tim Kennedy <timthreetwelve@outlook.com>
2024-04-24 21:04:27 -0500

    Fixed tab selection logic

--- TimVer/Views/DrivesPage.xaml.cs
+++ TimVer/Views/DrivesPage.xaml.cs
@@ -17,12 +17,17 @@ public partial class DrivesPage : UserControl
     #region Set active tab
     private void TabControl1_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-        TempSettings.Setting.DriveSelectedTab = TabControl1.SelectedIndex;
+        if (TabControl1.SelectedIndex != -1)
+        {
+            TempSettings.Setting.DriveSelectedTab = TabControl1.SelectedIndex;
+        }
     }
     #endregion Set active tab
 
+    #region Grid splitter drag completed event
     private void DetailsSplitter_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
     {
         UserSettings.Setting.DetailsHeight = Math.Floor(DetailsRow.Height.Value);
     }
+    #endregion Grid splitter drag completed event
 }
