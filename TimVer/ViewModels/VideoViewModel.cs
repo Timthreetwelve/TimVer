3e98adf8dacda8cb840a04b40a03cc3676bbddbb
Tim Kennedy <timthreetwelve@outlook.com>
2024-04-28 19:14:16 -0500

    Disabled warnings for empty classes.

--- TimVer/ViewModels/VideoViewModel.cs
+++ TimVer/ViewModels/VideoViewModel.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
 
 namespace TimVer.ViewModels;
+#pragma warning disable S2094 // Classes should not be empty
 internal class VideoViewModel
+#pragma warning restore S2094 // Classes should not be empty
+#pragma warning disable RCS1251 // Remove unnecessary braces from record declaration
 {
 }
+#pragma warning restore RCS1251 // Remove unnecessary braces from record declaration
--- TimVer/ViewModels/WindowsInfoViewModel.cs
+++ TimVer/ViewModels/WindowsInfoViewModel.cs
@@ -2,6 +2,10 @@
 
 namespace TimVer.ViewModels;
 
+#pragma warning disable S2094 // Classes should not be empty
 class WindowsInfoViewModel
+#pragma warning restore S2094 // Classes should not be empty
+#pragma warning disable RCS1251 // Remove unnecessary braces from record declaration
 {
 }
+#pragma warning restore RCS1251 // Remove unnecessary braces from record declaration
