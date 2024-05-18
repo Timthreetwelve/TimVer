ae94ce14b0b412707f1efa36d01880db3c725b54
Tim Kennedy <timthreetwelve@outlook.com>
2024-05-18 13:56:38 -0500

    For future use.

---
+++ TimVer/Helpers/SystemMetricsHelper.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
+
+using static Vanara.PInvoke.User32;
+
+namespace TimVer.Helpers;
+/// <summary>
+/// Class for GetSystemMetrics via P/Invoke.
+/// </summary>
+internal static class SystemMetricsHelper
+{
+    /// <summary>
+    /// Gets count of visible display monitors.
+    /// </summary>
+    /// <returns>Count of monitors as int.</returns>
+    public static int GetDisplayCount()
+    {
+        try
+        {
+            return GetSystemMetrics(SystemMetric.SM_CMONITORS);
+        }
+        catch (Exception ex)
+        {
+            _log.Error(ex, "Call to GetDisplayCount has failed.");
+            return -1;
+        }
+    }
+}
