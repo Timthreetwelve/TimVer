ac827db7fde22a87fb144cf02c706db319cbe669
Tim Kennedy <timthreetwelve@outlook.com>
2024-07-03 13:27:19 -0500

    Added XML comments to properties.

--- TimVer/Models/CommandLineOptions.cs
+++ TimVer/Models/CommandLineOptions.cs
@@ -4,6 +4,10 @@ namespace TimVer.Models;
 
 class CommandLineOptions
 {
+    /// <summary>
+    /// Option indicating whether or not to show the main window or record
+    /// build history and quit without showing the window.
+    /// </summary>
     [Option('h', "hide", Required = false)]
     public bool Hide { get; set; }
 }
