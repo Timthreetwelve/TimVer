337292f143a8c885d29474bfec2fa514cf7a899f
Tim Kennedy <timthreetwelve@outlook.com>
2024-07-03 13:28:15 -0500

    Added XML comments to properties.

--- TimVer/Models/LogicalDrives.cs
+++ TimVer/Models/LogicalDrives.cs
@@ -4,24 +4,45 @@ namespace TimVer.Models;
 
 public partial class LogicalDrives : ObservableObject
 {
+    /// <summary>
+    /// The name of the drive. e.g. C:
+    /// </summary>
     [ObservableProperty]
     private string? _name;
 
+    /// <summary>
+    /// The drive label.
+    /// </summary>
     [ObservableProperty]
     private string? _label;
 
+    /// <summary>
+    /// Type of drive. Fixed, removable, network, etc.
+    /// </summary>
     [ObservableProperty]
     private string? _driveType;
 
+    /// <summary>
+    /// The drive format. NTFS, FAT32, etc.
+    /// </summary>
     [ObservableProperty]
     private string? _format;
 
+    /// <summary>
+    /// Total size of the drive.
+    /// </summary>
     [ObservableProperty]
     private double? _totalSize;
 
+    /// <summary>
+    /// Available free space.
+    /// </summary>
     [ObservableProperty]
     private double? _gBFree;
 
+    /// <summary>
+    /// Free space expressed as a percentage.
+    /// </summary>
     [ObservableProperty]
     private double? _percentFree;
 }
