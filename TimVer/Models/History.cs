fe32e0d3b1a4914c0964ecc987cf9d2f0229bb8c
Tim Kennedy <timthreetwelve@outlook.com>
2024-07-03 13:28:05 -0500

    Added XML comments to properties.

--- TimVer/Models/History.cs
+++ TimVer/Models/History.cs
@@ -8,15 +8,27 @@ namespace TimVer.Models;
 public partial class History : ObservableObject
 {
     #region Properties
+    /// <summary>
+    /// Date the record was added to the history file.
+    /// </summary>
     [ObservableProperty]
     private string? _hDate;
 
+    /// <summary>
+    /// Windows build number.
+    /// </summary>
     [ObservableProperty]
     private string? _hBuild;
 
+    /// <summary>
+    /// Windows version.
+    /// </summary>
     [ObservableProperty]
     private string? _hVersion;
 
+    /// <summary>
+    /// Windows build branch.
+    /// </summary>
     [ObservableProperty]
     private string? _hBranch;
     #endregion Properties
