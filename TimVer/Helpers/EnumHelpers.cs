604344143df2a6d6dfd6ab8a1d7f4bb233d35930
Tim Kennedy <timthreetwelve@outlook.com>
2024-07-06 18:23:39 -0500

    Moved GetEnumDescription to new file in Helpers.

---
+++ TimVer/Helpers/EnumHelpers.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
+
+namespace TimVer.Helpers;
+
+internal static class EnumHelpers
+{
+    internal static string GetEnumDescription(Enum enumObj)
+    {
+        FieldInfo? field = enumObj.GetType().GetField(enumObj.ToString());
+        object[] attrArray = field!.GetCustomAttributes(false);
+
+        if (attrArray.Length > 0)
+        {
+            DescriptionAttribute? attribute = attrArray[0] as DescriptionAttribute;
+            return attribute!.Description;
+        }
+        else
+        {
+            return enumObj.ToString();
+        }
+    }
+}
