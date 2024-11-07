27d019457955750c931cd7d2376ec4eb0cdf0a22
Tim Kennedy <timthreetwelve@outlook.com>
2024-11-07 10:16:47 -0600

    Finished adding percent complete to translation contributors grid.

--- TimVer/ViewModels/AboutViewModel.cs
+++ TimVer/ViewModels/AboutViewModel.cs
@@ -4,6 +4,17 @@ namespace TimVer.ViewModels;
 
 public partial class AboutViewModel
 {
+    #region Constructor
+    public AboutViewModel()
+    {
+        if (AnnotatedLanguageList.Count == 0)
+        {
+            AddNote();
+        }
+    }
+    #endregion Constructor
+
+    #region Relay Commands
     [RelayCommand]
     private static void ViewLicense()
     {
@@ -32,4 +43,25 @@ public partial class AboutViewModel
     {
         await GitHubHelpers.CheckRelease();
     }
+    #endregion Relay Commands
+
+    #region Annotated Language translation list
+    public List<UILanguage> AnnotatedLanguageList { get; } = [];
+    #endregion Annotated Language translation list
+
+    #region Add note to list of languages
+    private void AddNote()
+    {
+        foreach (UILanguage item in UILanguage.DefinedLanguages)
+        {
+            // en-GB is a special case and therefore should not be listed.
+            // See the comments in Languages\Strings.en-GB.xaml.
+            if (item.LanguageCode is not "en-GB")
+            {
+                item.Note = GetLanguagePercent(item.LanguageCode!);
+                AnnotatedLanguageList.Add(item);
+            }
+        }
+    }
+    #endregion Add note to list of languages
 }
