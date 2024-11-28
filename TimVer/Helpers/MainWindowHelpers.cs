bbb68f3bd79df9caab839eab23b2368e826a62b1
Tim Kennedy <timthreetwelve@outlook.com>
2024-11-27 20:53:26 -0600

    Removed unneeded FindChild method and simplified code.

--- TimVer/Helpers/MainWindowHelpers.cs
+++ TimVer/Helpers/MainWindowHelpers.cs
@@ -144,7 +144,7 @@ internal static class MainWindowHelpers
         if (!CommandLineHelpers.UpdateHistoryOnly)
         {
             // Clear any remaining messages
-            Snackbar snackbar = FindChild<Snackbar>(Application.Current.MainWindow!, "SnackBar1");
+            Snackbar snackbar = _mainWindow!.SnackBar1;
             if (snackbar is not null)
             {
                 snackbar.MessageQueue!.Clear();
@@ -360,59 +360,6 @@ internal static class MainWindowHelpers
             return FindParent<T>(parentObject);
     }
     #endregion Find a parent of a control
-
-    #region Find a child of a control
-    /// <summary>
-    /// Finds a Child of a given item in the visual tree.
-    /// </summary>
-    /// <param name="parent">A direct parent of the queried item.</param>
-    /// <typeparam name="T">The type of the queried item.</typeparam>
-    /// <param name="childName">x:Name or Name of child. </param>
-    /// <returns>The first child item that matches the submitted type parameter.</returns>
-    private static T FindChild<T>(DependencyObject parent, string childName)
-       where T : DependencyObject
-    {
-        // Confirm parent and childName are valid. 
-        if (parent == null)
-            return null!;
-
-        T foundChild = null!;
-
-        int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
-        for (int i = 0; i < childrenCount; i++)
-        {
-            DependencyObject child = VisualTreeHelper.GetChild(parent, i);
-
-            // If the child is not of the request child type child
-            if (child is not T)
-            {
-                // recursively drill down the tree
-                foundChild = FindChild<T>(child, childName);
-
-                // If the child is found, break so we do not overwrite the found child. 
-                if (foundChild != null)
-                    break;
-            }
-            else if (!string.IsNullOrEmpty(childName))
-            {
-                // If the child's name is set for search
-                if (child is FrameworkElement frameworkElement && frameworkElement.Name == childName)
-                {
-                    // if the child's name is of the request name
-                    foundChild = (T)child;
-                    break;
-                }
-            }
-            else
-            {
-                // child element found.
-                foundChild = (T)child;
-                break;
-            }
-        }
-        return foundChild!;
-    }
-    #endregion Find a child of a control
 
     #region Change mouse pointer
     /// <summary>
