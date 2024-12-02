837d2440b1aff425ea812730a2cc7eafca347b89
Tim Kennedy <timthreetwelve@outlook.com>
2024-12-02 14:16:08 -0600

    Removed unneeded composite format.

--- TimVer/Helpers/ResourceHelpers.cs
+++ TimVer/Helpers/ResourceHelpers.cs
@@ -86,7 +86,6 @@ internal static class ResourceHelpers
     internal static CompositeFormat HardwareInfoUptimeString { get; } = GetCompositeResource("HardwareInfo_UptimeString");
     internal static CompositeFormat MsgTextAppUpdateNewerFound { get; } = GetCompositeResource("MsgText_AppUpdateNewerFound");
     internal static CompositeFormat MsgTextErrorOpeningFile { get; } = GetCompositeResource("MsgText_ErrorOpeningFile");
-    internal static CompositeFormat MsgTextErrorReadingFile { get; } = GetCompositeResource("MsgText_ErrorReadingFile");
     internal static CompositeFormat MsgTextFilterRowsShown { get; } = GetCompositeResource("MsgText_FilterRowsShown");
     internal static CompositeFormat MsgTextUIColorSet { get; } = GetCompositeResource("MsgText_UIColorSet");
     internal static CompositeFormat MsgTextUISizeSet { get; } = GetCompositeResource("MsgText_UISizeSet");
